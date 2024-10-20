8b8a101c90ff266fcbd5552c0abc587d3604f587
Thomas Helland <thomas.helland97@gmail.com>
2024-10-20 10:22:48 +0200

    omnisharp eglot

--- .emacs.d/elisp/th-eglot.el
+++ .emacs.d/elisp/th-eglot.el
@@ -27,21 +27,35 @@
 (use-package eglot
   :ensure t
   :hook
-  (((c-ts-mode c++-ts-mode) . eglot-ensure)
-   ((js2-mode js-ts-mode) . eglot-ensure)
-   ((html-mode web-mode) eglot-ensure)
-   ((css-ts-mode) eglot-ensure))
-   :config
-   (setq eglot-autoshutdown t)
-   (setq eglot-events-buffer-size 100000)
-   (add-to-list 'eglot-server-programs
-                '((js2-mode js-ts-mode) . ("/usr/bin/typescript-language-server" "--stdio"))) ;;; "--tsserver-inlay-hints"
-   (add-to-list 'eglot-server-programs
-                '((c-ts-mode c++-ts-mode) . ("clangd" "--clang-tidy" "--header-insertion=never" "--inlay-hints=true")))
-   (add-hook 'eglot-managed-mode-hook 'flymake-mode)
-   (add-hook 'eglot-managed-mode-hook 'eglot-inlay-hints-mode))
+  (c-ts-mode . eglot-ensure)
+  (c++-ts-mode . eglot-ensure)
+  (js2-mode . eglot-ensure)
+  (js-ts-mode . eglot-ensure)
+  (html-mode . eglot-ensure)
+  (web-mode . eglot-ensure)
+  (css-ts-mode . eglot-ensure)
+  (csharp-ts-mode . eglot-ensure)
+  (csharp-mode . eglot-ensure)
+  :config
+  (setq eglot-autoshutdown t)
+  (setq eglot-events-buffer-size 0)
+  (setq eglot-extend-to-xref t)
+  (setq eglot-verbose t)
+  (add-to-list 'eglot-server-programs
+               '((js2-mode js-ts-mode) . ("/usr/bin/typescript-language-server" "--stdio"))) ;;; "--tsserver-inlay-hints"
+  (add-to-list 'eglot-server-programs
+               '((c-ts-mode c++-ts-mode) . ("clangd" "--clang-tidy" "--header-insertion=never" "--inlay-hints=true")))
+  (add-to-list 'eglot-server-programs
+               '((csharp-mode) . ("/usr/bin/OmniSharp" "-lsp"))) ;;; "--stdio" "--verbose"
+  (add-hook 'eglot-managed-mode-hook 'flymake-mode)
+  (add-hook 'eglot-managed-mode-hook 'eglot-inlay-hints-mode))
 
 
+  ;; (((c-ts-mode c++-ts-mode) . eglot-ensure)
+  ;;  ((js2-mode js-ts-mode) . eglot-ensure)
+  ;;  ((html-mode web-mode) . eglot-ensure)
+  ;;  ((css-ts-mode) . eglot-ensure)
+  ;;  ((csharp-mode csharp-ts-mode) . eglot-ensure))
 
 
 
