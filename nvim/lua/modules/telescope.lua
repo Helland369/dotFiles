42a252150d5340601de31dbe3bc5d3f5674fd2ad
Thomas Helland <thomas.helland97@gmail.com>
2025-02-15 16:33:30 +0100

    Added more config to nvim telescope and tmux

--- nvim/lua/modules/telescope.lua
+++ nvim/lua/modules/telescope.lua
@@ -1,11 +1,19 @@
 return {
     "nvim-telescope/telescope.nvim",
-	dependencies = {"nvim-lua/plenary.nvim"},
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
     config = function ()
-        local telescope = require("telescope")
+        require("telescope").setup({
+            extensions = {
+                fzf = {}
+            }
+        })
+        require("telescope").load_extension("fzf")
+
         local builtin = require("telescope.builtin")
-		telescope.setup()
-		-- telescope leader keys
+        -- telescope leader keys
         vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
         vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
         vim.keymap.set("n", "<leader>bi", builtin.buffers, { desc = "List Buffers" })
