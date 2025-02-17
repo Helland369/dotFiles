2b81ee9cdbbc6d55403b1d7c743a9ef81f0beb11
Thomas Helland <thomas.helland97@gmail.com>
2025-02-17 21:50:04 +0100

    Can now make files and directories with telescope.

--- nvim/lua/modules/telescope.lua
+++ nvim/lua/modules/telescope.lua
@@ -3,20 +3,38 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        { "nvim-telescope/telescope-file-browser.nvim" }
     },
-    config = function ()
-        require("telescope").setup({
+    config = function()
+        local telescope = require("telescope")
+
+        telescope.setup({
             extensions = {
-                fzf = {}
+                fzf = {},
+                file_browser = {
+                    hijack_netrw = true,
+                    mappings = {
+                        ["i"] = {
+                            ["<C-n>"] = function(prompt_bufnr)
+                                local fb_actions = require("telescope").extensions.file_browser.actions
+                                fb_actions.create(prompt_bufnr)
+                            end,
+                        }
+                    }
+                }
             }
         })
-        require("telescope").load_extension("fzf")
+
+        telescope.load_extension("fzf")
+        telescope.load_extension("file_browser")
 
         local builtin = require("telescope.builtin")
-        -- telescope leader keys
+
         vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
         vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
         vim.keymap.set("n", "<leader>bi", builtin.buffers, { desc = "List Buffers" })
         vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help Tags" })
+
+        vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<CR>", { desc = "Open File Browser" })
     end
 }
